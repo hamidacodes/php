d96f09f857de7be8dc630c603577d078f57b83b7
hamidagayibkhanova <hamidagayibkhanova@gmail.com>
2026-04-09 22:58:43 +0400

    printr

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+   
+
+<?php
+
+
+echo "<pre>";
+print_r(
+    [
+        'ad' => 'Ali',
+        'soyad' => 'Veli',
+        'yas' => 30
+    ]
+);
+echo "<pre>";
+
+?>
+
+<!-- <script>
+let a = {
+    'ad': 'Ali'
+    'soyad': 'Veli'
+    'yas': 30
+}
+</script> -->
+
+</body>
+</html>
