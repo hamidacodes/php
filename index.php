8c98ac3238385f830a0c9f40dc04f2bd16d0c41c
hamidagayibkhanova <hamidagayibkhanova@gmail.com>
2026-04-10 16:59:21 +0400

    comprasion operations

--- index.php
+++ index.php
@@ -96,28 +96,66 @@
     // echo gettype($var4) . "<br>";
     // echo gettype($var5) . "<br>";
 
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+  
 
-    //    OPEARTORLAR
-    // toplama
-   $result1 =5+3;  
 
-//    cixma
-   $result2 =5-3;
 
-//    vurma
-   $result3 =5*3;
+// //    OPEARTORLAR
+//     // toplama
+//    $result1 =5+3;  
 
-//    bolme
-   $result4 =5/3;
+// //    cixma
+//    $result2 =5-3;
 
-//    qaliqli bolme
-   $result5 =5%3;
+// //    vurma
+//    $result3 =5*3;
 
-   print ("Toplama:" . $result1 . "<br>");
-   print ("Cixma:" . $result2 . "<br>");
-   print ("Vurma:" . $result3 . "<br>");
-   print ("Bolme:" .  $result4 . "<br>");
-   print ("Mod:" .  $result5 . "<br>");
+// //    bolme
+//    $result4 =5/3;
+
+// //    qaliqli bolme
+//    $result5 =5%3;
+
+//    print ("Toplama:" . $result1 . "<br>");
+//    print ("Cixma:" . $result2 . "<br>");
+//    print ("Vurma:" . $result3 . "<br>");
+//    print ("Bolme:" .  $result4 . "<br>");
+//    print ("Mod:" .  $result5 . "<br>");
+
+// $result6 bərabərlik operatoru, dəyərlər bərabərdirsə true, deyilsə false qaytarır
+// $result7 eynilik (identik) operatoru, dəyərlər və tiplər eynidirsə true, deyilsə false qaytarır
+// $result8 bərabər deyil operatoru, dəyərlər fərqlidirsə true, eynidirsə false qaytarır
+// $result9 eyni deyil (identik deyil) operatoru, dəyərlər və tiplər fərqlidirsə true, eynidirsə false qaytarır
+// $result10 böyükdür operatoru, dəyərlərdən biri digərindən böyükdürsə true, əks halda false qaytarır
+// $result11 kiçikdir operatoru, dəyərlərdən biri digərindən kiçikdirsə true, əks halda false qaytarır
+// $result12 böyük və ya bərabərdir operatoru, böyük və ya bərabərdirsə true, kiçikdirsə false qaytarır
+// $result13  kiçik və ya bərabərdir operatoru, kiçik və ya bərabərdirsə true, böyükdürsə false qaytarır
+// $result14  // spaceship operatoru, əgər dəyərlər bərabərdirsə 0 qaytarır,birinci dəyər ikinci dəyərdən böyükdürsə 1 qaytarır,birinci dəyər ikinci dəyərdən kiçikdirsə -1 qaytarır
+
+
+     $result6 =(5 == 5);
+     $result7 =(5 === "5");
+     $result8 =(5 != 3);
+     $result9 =(5 !== "5");
+     $result10 =(5 > 3);
+     $result11 =(5 < 3);
+     $result12 =(5 >= 5);
+     $result13 =(5 <= 3);
+
+      $result14 =(5 <=> 3);
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
