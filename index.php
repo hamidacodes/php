b620b3b5909e9438ed9ec172bb7115ec3a61f226
hamidagayibkhanova <hamidagayibkhanova@gmail.com>
2026-05-06 18:17:12 +0400

    class

--- index.php
+++ index.php
@@ -82,15 +82,57 @@
 
     // ureyinIstiyenQeder("armud","banana","portaqal","ciyelek","feyxoa")
 
-function printLanguages($a,$b,$c){
-        echo "Programming languages: $a,$b,$c <br>";
-     }
+// function printLanguages($a,$b,$c){
+//         echo "Programming languages: $a,$b,$c <br>";
+//      }
 
-     $Languages=["Phython","JavaScript","PHP"];
+//      $Languages=["Phython","JavaScript","PHP"];
 
-    printLanguages(...$Languages);
+//     printLanguages(...$Languages);
+
+<?php
 
 
+   class Car {
+
+    // propery
+      public $color;
+      public $model;
+
+
+
+    //  metod
+      public function start() {
+        echo "Avtomobil ise dusdu. <hr>";
+      }
+   }
+
+   $bmw = new Car ();
+   $bmw->color="qirmizi";
+   $bmw->model="Bmw X5";
+
+   echo $bmw->color . "<br>";
+   echo $bmw->model . "<br>";
+    $bmw->start();
+
+   $mercedes = new Car ();
+   $mercedes->color="boz";
+   $mercedes->model="mercedes S-class";
+
+   echo $mercedes->color . "<br>";
+   echo $mercedes->model . "<br>";
+    $mercedes->start();
+
+
+
+
+    $dodge = new Car ();
+    $dodge->color="qara";
+    $dodge->model="dodge hellcat";
+
+    echo $dodge->color . "<br>";
+    echo $dodge->model . "<br>";
+    $dodge->start();
     
 
 
@@ -965,7 +1007,7 @@ function printLanguages($a,$b,$c){
 
 ?>
 
-<!-- // <script>
+// <!-- // <script>
 // let a = {
 //     'ad': 'Ali'
 //     'soyad': 'Veli'
